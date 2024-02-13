eb694410875b575f8130d3614239815bc8b295ab
Amabelle Trachtenberg <amabelletr@gmail.com>
2024-02-13 21:34:30 +0200

    Added lottie

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import Player from '@/componenets/lotties/animation'
+import Search from '@/componenets/search-input/search'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Store And Share</h1>  
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="grid text-center p-8">
+      <h1 className='main-header'>Store And Share</h1>  
+      <Player/>
+      <p className=''>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+      <Search placeholder='Search Your Items'/>
     </main>
   );
 }
---
+++ componenets/search-input/search.tsx
@@ -0,0 +1,38 @@
+'use client';
+ 
+// import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+ 
+export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSearch(term: string) {
+    console.log(term);
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
+ 
+  return (
+    <div className="grid p-24">
+      {/* <label htmlFor="search" className="input-label">
+        Search
+      </label> */}
+      <input
+        className="input-field"
+        placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
+      />
+      {/* <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
+    </div>
+  );
+}
